ddd996a4d2d6c27257ead99eb2e65b63b46006c2
LucasFeli <felipe298@gmail.com>
2021-02-06 00:12:14 +0100

    2chng

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+export function Footer ({children}){
+    return(
+        <>
+            {children}
+            
+        </>
+    )
+}
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,55 +1,61 @@
-import {data} from "..//data/data"
-import {Navbar} from "../components/Navbar"
-import {Hero} from "../components/Hero"
+import { data } from "..//data/data";
+import { Navbar } from "../components/Navbar";
+import { Hero } from "../components/Hero";
 import { Body } from "../components/Body";
+import {Footer} from "../components/Footer"
 
-export function Main(){
-    return(
-        <main>
-            <Navbar>
-                {navbarLinks(data)}
-            </Navbar> 
-            <Hero>
-                 {heroLinks(data)}
-            </Hero>
-            <Body>
-                {BodyLinks(data)}
-            </Body>
 
-        </main>
-    )
+export function Main() {
+  return (
+    <main>
+      <Navbar> Navbar {navbarLinks(data)}</Navbar>
+      <Hero> Hero {heroLinks(data)}</Hero>
+      <Body> body {bodyLinks(data)}</Body>
+      <Footer> footer {footer(data)}</Footer>
+     
+    </main>
+  );
 }
 
-
-function navbarLinks({navbar}){
-    return(
-        <>
-           <h1>{ navbar.title}</h1> 
-         
-        </>
-        )
-
+function navbarLinks({ navbar }) {
+    
+    
+    return (
+    <>
+      <h1>{navbar.title}</h1>
+     
+    </>
+  );
 }
 
-function heroLinks({hero}){
-    return(
-        <>
-           <h1>{ hero.title}</h1> 
-           <h1>{ hero.subtitle}</h1> 
-         
-        </>
-        )
-
+function heroLinks({ hero }) {
+  return (
+      
+    <>
+      <h1>{hero.title}</h1>
+      <h1>{hero.subtitle}</h1>
+    </>
+  );
 }
 
-function BodyLinks({body}){
-    return(
-        <>
-           
-          
-         
-        </>
-        )
+function bodyLinks({ body }) {
+  return <>
+    <h1>{body.utilsSection.sectionTitle}</h1>
+    <h1>{body.developersSection.sectionTitle}</h1>
+    <h1>{body.developersSection.desctionDescription}</h1>
 
+    
+  </>;
 }
 
+function footer({ footer }) {
+    return <>
+        <h1>{footer.contact.email}</h1>
+        <h1>{footer.contact.phone}</h1>
+        <h1>{footer.contact.address.country}</h1>
+        <h1>{footer.contact.address.street}</h1>
+        <h1>{footer.contact.address.state}</h1>
+    </>;
+  }
+  
+  
